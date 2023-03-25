ab1e3a1209f9d2dc86aa0796e987a64287ece492
roisatff <roistaff1983@gmail.com>
2023-03-25 19:36:33 +0900

    new git

---
+++ kmc.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gvalkov/golang-evdev"
+	"os"
+	"strings"
+)
+
+func search() string {
+	devices, err := evdev.ListInputDevices()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, dev := range devices {
+		fmt.Printf("Device name: %s, FN: %s\n", dev.Name, dev.Fn)
+		if strings.Contains(dev.Name, "Keyboard") == true {
+			devicepath := dev.Fn
+			return devicepath
+		}
+	}
+	return ""
+}
+
+func Count(path string) int {
+	var count int
+	if path == "" {
+		panic("Keyboard device not found")
+	}
+	dev, err := evdev.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer dev.File.Close()
+	for {
+		events, err := dev.Read()
+		if err != nil {
+			panic(err)
+		}
+		for i := range events {
+			event := &events[i]
+			if event.Type == evdev.EV_KEY {
+				keyCode := event.Code
+				keyValue := event.Value
+				if keyValue != 0 {
+					count++
+				}
+				if keyCode == 14 {
+					fmt.Printf("\r backspace key. You made %d mistakes.", count/2)
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	path := search()
+	Count(path)
+}
+
