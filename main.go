f51503e634b144aba5a051cc326ef5762a15f8ce
roisatff <roistaff1983@gmail.com>
2023-03-27 15:58:40 +0900

    update func,design,etc

--- main.go
+++ main.go
@@ -5,10 +5,24 @@ import (
 	"strings"
 	"github.com/gvalkov/golang-evdev"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
+	time.Sleep(time.Second * 1)
 	fmt.Println("\033[1m","Keyboard Mistake Counter is running...","\033[0m")
+	sigs := make(chan os.Signal,1)
+	signal.Notify(sigs,syscall.SIGINT,syscall.SIGTERM)
+	go func(){
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println("   ",sig)
+		message := "   "+"\033[32m"+"○"+"\033[0m"+" Ran successfully."
+		fmt.Println(message)
+		os.Exit(0)
+	}()
 	devices, err := evdev.ListInputDevices()
 	if err != nil {
 		panic(err)
@@ -37,6 +51,7 @@ func main() {
 				fmt.Printf("\033[2K\r   You made %s mistakes!", counter)
 				if event.Value == 1 {
 					if event.Code == 14{
+					time.Sleep(time.Millisecond * 600)
 					count++
 				}
 				} else if event.Value == 2 {
@@ -48,4 +63,3 @@ func main() {
 		}
 	}
 }
-
